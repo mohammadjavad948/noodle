ceea9da01643e68ecb7a855872c5004d8261827e
mohammad javad <mjkmo123@gmail.com>
2021-03-14 17:03:49 +0330

    add router

--- artorito/src/Main.tsx
+++ artorito/src/Main.tsx
@@ -1,11 +1,17 @@
 import React from "react";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Switch} from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 export default function Main() {
     return (
         <BrowserRouter>
             <Navbar />
+
+            <div className="container">
+                <Switch>
+
+                </Switch>
+            </div>
         </BrowserRouter>
     )
 }
