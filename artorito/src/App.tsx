2ef3d6ab2daee5b9a96b5e8c12d73421e7ac7ecc
mohammad javad <mjkmo123@gmail.com>
2021-03-14 10:06:36 +0330

    create login component

--- artorito/src/App.tsx
+++ artorito/src/App.tsx
@@ -5,8 +5,11 @@ import {Button} from "@material-ui/core";
 import Navbar from "./components/Navbar";
 import ConnectionLost from "./components/ConnectionLost";
 import {useConnectionStore} from "./stores/ConnectionStore";
+import {useTokenStore} from "./stores/TokenStore";
 
 function App() {
+
+    const {token} =  useTokenStore();
 
     return (
         <div>
