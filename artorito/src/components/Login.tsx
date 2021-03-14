9ad1df1bc06569c3926cf04577c21dc3b87ec8fc
mohammad javad <mjkmo123@gmail.com>
2021-03-14 13:00:38 +0330

    add css modules

--- artorito/src/components/Login.tsx
+++ artorito/src/components/Login.tsx
@@ -2,36 +2,9 @@ import React from "react";
 import {Button, makeStyles, TextField, Typography} from "@material-ui/core";
 import {useConnectionStore} from "../stores/ConnectionStore";
 import {useSpring, animated} from "react-spring";
-
-const useStyles = makeStyles({
-    main: {
-        width: '100%',
-        height: '100vh',
-        overflow: 'hidden',
-        backgroundImage: 'url(/background.jpg)',
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    login: {
-        maxWidth: '400px',
-        width: '100%',
-        height: '300px',
-        background: '#151515',
-        borderRadius: '8px',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        gap: '20px'
-    }
-});
+import styles from './login.module.css';
 
 export default function Login() {
-    const styles = useStyles();
     const {status: connectionStatus} = useConnectionStore();
     const [loginProps, set] = useSpring(() => ({x: 0, y: 0}));
 
