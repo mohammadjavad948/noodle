710d377bb642706e14b8412a293d081500f39c83
mohammad javad <mjkmo123@gmail.com>
2021-03-14 13:33:55 +0330

    create login states

--- artorito/src/components/Login.tsx
+++ artorito/src/components/Login.tsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import {Button, TextField, Typography} from "@material-ui/core";
 import {useConnectionStore} from "../stores/ConnectionStore";
 import {useSpring, animated} from "react-spring";
 import styles from './login.module.css';
 
 export default function Login() {
+    // animations and connection checks
     const {status: connectionStatus} = useConnectionStore();
     const [loginProps, set] = useSpring(() => ({x: 0, y: 0}));
 
     function Mouse(event: any){
        set({x: event.pageX, y: event.pageY})
     }
+
+    // login logic
+    const [requesting, setRequest] = useState(false);
+    const [username, setUsername] = useState('')
+    const [passowrd, setPassword] = useState('')
 
     return (
         <div className={styles.main} onMouseMove={Mouse}>
@@ -25,7 +31,7 @@ export default function Login() {
                 <Typography variant={'h6'}>Welcome</Typography>
                 <TextField label="username" variant={"outlined"}/>
                 <TextField label="password" type="password" variant={"outlined"}/>
-                <Button disabled={!connectionStatus} variant={"contained"} color={"secondary"}>login</Button>
+                <Button disabled={!connectionStatus && requesting} variant={"contained"} color={"secondary"}>login</Button>
             </animated.div>
         </div>
     )
