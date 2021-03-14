cfbbef06d6e536afe0880089ae724fa89b64c341
mohammad javad <mjkmo123@gmail.com>
2021-03-14 14:02:13 +0330

    remove unused import

--- artorito/src/App.tsx
+++ artorito/src/App.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Router, Switch} from "react-router-dom";
-import {Button} from "@material-ui/core";
-import Navbar from "./components/Navbar";
 import ConnectionLost from "./components/ConnectionLost";
-import {useConnectionStore} from "./stores/ConnectionStore";
 import {useTokenStore} from "./stores/TokenStore";
 import Login from "./components/Login";
 import Main from "./Main";
