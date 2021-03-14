1b081b939ae5cebe994c9e3bcb5ebad29614c567
mohammad javad <mjkmo123@gmail.com>
2021-03-14 11:17:23 +0330

    add some thick animation

--- artorito/src/components/Login.tsx
+++ artorito/src/components/Login.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import {Button, makeStyles, TextField, Typography} from "@material-ui/core";
+import {useConnectionStore} from "../stores/ConnectionStore";
+import {useSpring, animated} from "react-spring";
 
 const useStyles = makeStyles({
     main: {
@@ -30,14 +32,28 @@ const useStyles = makeStyles({
 
 export default function Login() {
     const styles = useStyles();
+    const {status: connectionStatus} = useConnectionStore();
+    const [loginProps, set] = useSpring(() => ({x: 0, y: 0}));
+
+    function Mouse(event: any){
+       set({x: event.pageX, y: event.pageY})
+    }
+
     return (
-        <div className={styles.main}>
-            <div className={styles.login}>
+        <div className={styles.main} onMouseMove={Mouse}>
+            <animated.div
+                className={styles.login}
+                style={{
+                    // @ts-ignore
+                    marginLeft: loginProps.x.interpolate(v => v/10 + 'px'),
+                    // @ts-ignore
+                    marginTop: loginProps.y.interpolate(v => v/10 + 'px')
+                }}>
                 <Typography variant={'h6'}>Welcome</Typography>
                 <TextField label="username" variant={"outlined"}/>
                 <TextField label="password" type="password" variant={"outlined"}/>
-                <Button variant={"contained"} color={"secondary"}>login</Button>
-            </div>
+                <Button disabled={!connectionStatus} variant={"contained"} color={"secondary"}>login</Button>
+            </animated.div>
         </div>
     )
 }
