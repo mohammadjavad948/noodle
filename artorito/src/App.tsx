d6026112ca9258052133636b9fa9f216f615203d
mohammad javad <mjkmo123@gmail.com>
2021-03-15 10:27:07 +0330

    add theme provider to app

--- artorito/src/App.tsx
+++ artorito/src/App.tsx
@@ -4,15 +4,28 @@ import ConnectionLost from "./components/ConnectionLost";
 import {useTokenStore} from "./stores/TokenStore";
 import Login from "./components/Login";
 import Main from "./Main";
+import {createMuiTheme, CssBaseline, ThemeProvider} from "@material-ui/core";
+import {useThemeStore} from "./stores/ThemeStore";
 
 function App() {
 
     const {token} =  useTokenStore();
+    const {theme: themeSaved} = useThemeStore();
+
+    const theme = createMuiTheme({
+        palette: {
+            // @ts-ignore
+            type: themeSaved
+        }
+    });
 
     return (
         <div>
-            {token !== '' ? <Main /> : <Login />}
-            <ConnectionLost />
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                {token !== '' ? <Main /> : <Login />}
+                <ConnectionLost />
+            </ThemeProvider>
         </div>
     )
 }
--- artorito/src/index.tsx
+++ artorito/src/index.tsx
@@ -3,21 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {ThemeProvider, createMuiTheme} from '@material-ui/core';
-import CssBaseline from '@material-ui/core/CssBaseline';
 
-const theme = createMuiTheme({
-    palette: {
-        type: "light"
-    }
-});
 
 ReactDOM.render(
   <React.StrictMode>
-      <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <App />
-      </ThemeProvider>
+      <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
