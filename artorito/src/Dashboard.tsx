379106e685e330daa25def929ba88f7f67147b04
mohammad javad <mjkmo123@gmail.com>
2021-03-15 09:48:57 +0330

    basic chart

--- artorito/src/Dashboard.tsx
+++ artorito/src/Dashboard.tsx
@@ -1,12 +1,18 @@
-import React from "react";
-import {Button, Card, CardActions, CardContent, Typography} from "@material-ui/core";
-import {Link} from 'react-router-dom';
+import React, {useEffect, useRef} from "react";
+import {Card, CardContent, Typography} from "@material-ui/core";
+import Chart from "chart.js";
+
+Chart.defaults.global.defaultFontFamily = "'PT Sans', sans-serif"
+// @ts-ignore
+Chart.defaults.global.legend.display = false;
 
 export default function Dashboard() {
 
     return (
-        <div className="row">
-            <DashboardCard title="Quiz" description="View All Quizzes"/>
+        <div className="row" style={{gap: '10px'}}>
+            <DashboardCard title="21 H" description="Math"/>
+            <DashboardCard title="1 H" description="Biology"/>
+            <DashboardCard title="10 H" description="Computer"/>
         </div>
     )
 }
@@ -17,8 +23,53 @@ interface DashboardCardI {
 }
 
 function DashboardCard(props: DashboardCardI) {
+
+    const graphCanvas = useRef() as React.MutableRefObject<HTMLCanvasElement>;
+
+    useEffect(() => {
+       buildChart();
+    });
+
+    function buildChart() {
+        let canvas = graphCanvas.current.getContext('2d');
+
+        // @ts-ignore
+        new Chart(canvas, {
+            type: 'line',
+            data: {
+                labels: [1, 2, 3, 4, 5],
+                datasets: [{
+                    label: props.title,
+                    fill: false,
+                    data: [50, 80, 20, 30, 100]
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        gridLines: {
+                            display: false
+                        },
+                        ticks: {
+                            beginAtZero: true,
+                            display: false
+                        }
+                    }],
+                    xAxes: [{
+                        gridLines: {
+                            display: false,
+                        },
+                        ticks: {
+                            display: false
+                        }
+                    }],
+                }
+            }
+        })
+    }
+
     return (
-        <Card variant={"outlined"} className="col-12 col-sm-12 col-md-6 col-lg-3 col-xxl-2">
+        <Card variant={"outlined"} className="col-12 col-sm-12 col-md-5 col-lg-3 col-xxl-3">
             <CardContent>
                 <Typography style={{textAlign: 'center', fontSize: '20px'}}>
                     {props.title}
@@ -26,6 +77,10 @@ function DashboardCard(props: DashboardCardI) {
                 <Typography style={{textAlign: 'center', fontSize: '14px', opacity: '60%'}}>
                     {props.description}
                 </Typography>
+                <canvas
+                    width={"100%"}
+                    ref={graphCanvas}
+                />
             </CardContent>
         </Card>
     )
