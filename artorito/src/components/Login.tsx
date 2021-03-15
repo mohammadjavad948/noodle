bf0997bf3f6f0dc45d78bf9276ce7d81236fc0ba
mohammad javad <mjkmo123@gmail.com>
2021-03-15 16:02:18 +0330

    add sign up and a bunch of transition

--- artorito/src/components/Login.tsx
+++ artorito/src/components/Login.tsx
@@ -1,24 +1,144 @@
 import React, {useEffect, useState} from "react";
 import {Button, FormControlLabel, Switch, TextField, Typography, useTheme} from "@material-ui/core";
 import {useConnectionStore} from "../stores/ConnectionStore";
-import {useSpring, animated} from "react-spring";
+import {useSpring, animated, useSprings} from "react-spring";
 import styles from './login.module.css';
 import Spinner from "./Spinner";
 import {useTokenStore} from "../stores/TokenStore";
 import {useThemeStore} from "../stores/ThemeStore";
 
-export default function Login() {
-    // animations and connection checks
+
+// @ts-ignore
+function LoginFrom({setAnimation, signUp, style}){
+
     const {status: connectionStatus} = useConnectionStore();
     const {set: setToken} = useTokenStore();
-    const {theme: savedTheme, light, dark} = useThemeStore();
 
+    // login logic
+    const [requesting, setRequest] = useState(false);
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    function usernameChanged(event: any){
+        setUsername(event.target.value);
+    }
+    function passwordChanged(event: any){
+        setPassword(event.target.value);
+    }
+    function login(){
+        setRequest(true);
+
+        setTimeout(()=> {
+            setAnimation({opacity: 0})
+        }, 2500)
+        setTimeout(()=> {
+            setToken('aefefesf');
+        }, 3000)
+    }
+
+    return (
+        <animated.div style={style} className={styles.loginForm}>
+            <Typography variant={'h6'}>Welcome</Typography>
+            <TextField label="username" variant={"outlined"} onChange={usernameChanged}/>
+            <TextField label="password" type="password" variant={"outlined"} onChange={passwordChanged}/>
+            <div>
+                <Button
+                    disabled={!connectionStatus || requesting}
+                    onClick={login}
+                    variant={"contained"}
+                    color={"secondary"}
+                >
+                    {requesting ? <Spinner /> : 'login'}
+                </Button>
+                <Button
+                    disabled={!connectionStatus || requesting}
+                    onClick={signUp}
+                    size="small"
+                >
+                    or signup
+                </Button>
+            </div>
+        </animated.div>
+    )
+}
+
+
+// @ts-ignore
+function SignUpForm({setAnimation, login, style}){
+
+    const {status: connectionStatus} = useConnectionStore();
+    const {set: setToken} = useTokenStore();
+
+    // login logic
+    const [requesting, setRequest] = useState(false);
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [name, setName] = useState('')
+
+    function usernameChanged(event: any){
+        setUsername(event.target.value);
+    }
+    function nameChanged(event: any){
+        setName(event.target.value);
+    }
+    function passwordChanged(event: any){
+        setPassword(event.target.value);
+    }
+    function signUp(){
+        setRequest(true);
+
+        setTimeout(()=> {
+            setAnimation({opacity: 0})
+        }, 2500)
+        setTimeout(()=> {
+            setToken('aefefesf');
+        }, 3000)
+    }
+
+    return (
+        <animated.div style={style} className={styles.loginForm}>
+            <Typography variant={'h6'}>Welcome</Typography>
+            <TextField label="name" variant={"outlined"} onChange={nameChanged}/>
+            <TextField label="username" variant={"outlined"} onChange={usernameChanged}/>
+            <TextField label="password" type="password" variant={"outlined"} onChange={passwordChanged}/>
+           <div>
+               <Button
+                   disabled={!connectionStatus || requesting}
+                   onClick={signUp}
+                   variant={"contained"}
+                   color={"secondary"}
+               >
+                   {requesting ? <Spinner /> : 'sign up'}
+               </Button>
+               <Button
+                   disabled={!connectionStatus || requesting}
+                   onClick={login}
+                   size="small"
+               >
+                   or login
+               </Button>
+           </div>
+        </animated.div>
+    )
+}
+
+export default function Login() {
+    // animations and connection checks
+    const {theme: savedTheme, light, dark} = useThemeStore();
     const [loginProps, set] = useSpring(() => ({
         x: 0,
         y: 0,
         opacity: 0,
+        height: '300px',
         background: savedTheme === 'light' ? '#e9e9e9' : '#151515'
     }));
+
+    const [springs, setPage] = useSprings(2, (index) => {
+        return {
+            position: 'absolute',
+            right: index === 1 ? '420px' : '90px'
+        }
+    });
 
     useEffect(() => {
         set({opacity: 100});
@@ -39,26 +159,30 @@ export default function Login() {
         }
     }
 
-    // login logic
-    const [requesting, setRequest] = useState(false);
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-
-    function usernameChanged(event: any){
-        setUsername(event.target.value);
-    }
-    function passwordChanged(event: any){
-        setPassword(event.target.value);
-    }
     function login(){
-        setRequest(true);
+        console.log('login');
+        // @ts-ignore
+        setPage((index) => {
+            console.log(index)
+            return {
+                position: 'absolute',
+                right: index === 1 ? '420px' : '90px'
+            }
+        })
+        set({height: '300px'})
+    }
 
-        setTimeout(()=> {
-            set({opacity: 0})
-        }, 2500)
-        setTimeout(()=> {
-            setToken('aefefesf');
-        }, 3000)
+    function signUp(){
+        console.log('sign up')
+        // @ts-ignore
+        setPage((index) => {
+            console.log(index)
+            return {
+                position: 'absolute',
+                right: index === 0 ? '420px' : '90px'
+            }
+        })
+        set({height: '420px'})
     }
 
     return (
@@ -71,22 +195,20 @@ export default function Login() {
                     // @ts-ignore
                     marginBottom: loginProps.y.interpolate(v => v/10 + 'px'),
                     opacity: loginProps.opacity.interpolate(v => v + '%'),
-                    background: loginProps.background
+                    background: loginProps.background,
+                    height: loginProps.height
                 }}>
-                <Typography variant={'h6'}>Welcome</Typography>
-                <TextField label="username" variant={"outlined"} onChange={usernameChanged}/>
-                <TextField label="password" type="password" variant={"outlined"} onChange={passwordChanged}/>
-                <Button
-                    disabled={!connectionStatus || requesting}
-                    onClick={login}
-                    variant={"contained"}
-                    color={"secondary"}
-                >
-                    {requesting ? <Spinner /> : 'login'}
-                </Button>
+                {springs.map((props, i) => {
+                    return i === 0 ?
+                        // @ts-ignore
+                        <LoginFrom key={i} setAnimation={set} signUp={signUp} style={props}/>
+                        // @ts-ignore
+                        : <SignUpForm key={i} setAnimation={set} login={login} style={props}/>
+                })}
                 <FormControlLabel
                     control={<Switch size="small" checked={savedTheme === 'dark'} color={"primary"} onChange={changeTheme}/>}
                     label="dark"
+                    style={{position: 'absolute', bottom: '5px'}}
                 />
             </animated.div>
         </div>
