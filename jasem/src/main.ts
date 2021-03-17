588b06fc4ac647413b9060908324f7dbda4f8777
mohammad javad <mjkmo123@gmail.com>
2021-03-17 14:30:08 +0330

    connect to mongo

--- jasem/src/main.ts
+++ jasem/src/main.ts
@@ -1,9 +1,14 @@
 import * as express from 'express';
 import {Socket} from "socket.io";
+import {connect} from "mongoose";
 const socketIo = require('socket.io');
 
 const app = express();
 
+
+connect(process.env.DB || 'mongodb://admin:secret@localhost:27017/noodle?authSource=admin', {useUnifiedTopology: true, useNewUrlParser: true}, () => {
+    console.log('connected to db')
+})
 
 const server = app.listen(process.env.PORT || 3000, () => {
     console.log('server is running')
