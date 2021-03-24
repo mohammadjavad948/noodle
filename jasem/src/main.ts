5531640bf613ec5f7d41595f337a47df5e4e935c
mohammad javad <mjkmo123@gmail.com>
2021-03-24 16:50:07 +0430

    if user exists throws error

--- jasem/src/main.ts
+++ jasem/src/main.ts
@@ -1,11 +1,17 @@
 import * as express from 'express';
 import {connect} from "mongoose";
 import * as bodyParser from "body-parser";
+import {User} from "./mongo/User";
 
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/register', (req, res) => {
+app.post('/register', async (req, res) => {
+    const {username, password, name} = req.body;
+
+    const user = await User.find({username});
+
+    if (user) return res.status(400).send({message: 'username exists!'});
 
 });
 
