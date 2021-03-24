8e07591dc5d5d6e4602f12c08522cea576cd7bfa
mohammad javad <mjkmo123@gmail.com>
2021-03-24 16:41:09 +0430

    add body parser

--- jasem/src/main.ts
+++ jasem/src/main.ts
@@ -1,8 +1,13 @@
 import * as express from 'express';
 import {connect} from "mongoose";
+import * as bodyParser from "body-parser";
 
 const app = express();
+app.use(bodyParser.json());
 
+app.post('/register', (req, res) => {
+
+});
 
 connect(process.env.DB || 'mongodb://admin:secret@localhost:27017/noodle?authSource=admin', {useUnifiedTopology: true, useNewUrlParser: true}, () => {
     console.log('connected to db')
