cb445f6f7c3f1b1c917b1ff6510b5ce875dd3900
mohammad javad <mjkmo123@gmail.com>
2021-03-24 16:11:21 +0430

    finished label model

---
+++ jasem/src/mongo/Label.ts
@@ -0,0 +1,14 @@
+import {Document, model, Schema} from "mongoose";
+
+interface LabelI extends Document{
+    name: string
+    color: string
+}
+
+const schema = new Schema({
+    name: String,
+    color: String
+});
+
+
+export const Label = model<LabelI>('label', schema);
