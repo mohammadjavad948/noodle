336dc10f54af74f8815ebf0c43bc51874e23661a
mohammad javad <mjkmo123@gmail.com>
2021-03-24 18:32:28 +0430

    add dotenv

--- jasem/src/main.ts
+++ jasem/src/main.ts
@@ -2,17 +2,32 @@ import * as express from 'express';
 import {connect} from "mongoose";
 import * as bodyParser from "body-parser";
 import {User} from "./mongo/User";
+import {genSalt, hash} from "bcrypt";
+require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.json());
 
 app.post('/register', async (req, res) => {
-    const {username, password, name} = req.body;
+    const {username, password: pass, name} = req.body;
 
     const user = await User.find({username});
 
     if (user) return res.status(400).send({message: 'username exists!'});
 
+    // create new user
+    const password = await hash(pass, await genSalt(10));
+
+    const newUser = await User.create({
+        username, name, password
+    });
+
+    // generate token
+    const token
+
+    return res.send({
+        user: newUser
+    })
 });
 
 connect(process.env.DB || 'mongodb://admin:secret@localhost:27017/noodle?authSource=admin', {useUnifiedTopology: true, useNewUrlParser: true}, () => {
