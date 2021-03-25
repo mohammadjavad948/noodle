9dde5c97e31b0a1ea8fc65f0925704642e796576
mohammad javad <mjkmo123@gmail.com>
2021-03-25 08:51:06 +0430

    move functions

--- jasem/src/auth.ts
+++ jasem/src/auth.ts
@@ -1,6 +1,7 @@
 import {Request, Response} from "express";
-import {verify} from "jsonwebtoken";
+import {sign, verify} from "jsonwebtoken";
 import {User} from "./mongo/User";
+import {compare, genSalt, hash} from "bcrypt";
 
 
 export async function authMiddleware(req: Request, res: Response, next){
@@ -21,4 +22,46 @@ export async function authMiddleware(req: Request, res: Response, next){
     }catch (e){
         res.status(403).send({message: 'auth error'});
     }
+}
+
+export async function login(req: Request, res: Response){
+    const {username, password} = req.body;
+
+    const user = await User.findOne({username});
+
+    if (!user) return res.status(400).send({message: 'wrong info'});
+
+    // check for password
+    const passwordIsTrue = await compare(password, user.password);
+
+    if (!passwordIsTrue) return res.status(400).send({message: 'wrong info'});
+
+    // generate token
+    const token = await sign({_id: user._id}, process.env.TOKEN);
+
+    return res.send({
+        user: user, token
+    })
+}
+
+export async function register(req: Request, res: Response) {
+    const {username, password: pass, name} = req.body;
+
+    const user = await User.findOne({username});
+
+    if (user) return res.status(400).send({message: 'username exists!'});
+
+    // create new user
+    const password = await hash(pass, await genSalt(10));
+
+    const newUser = await User.create({
+        username, name, password
+    });
+
+    // generate token
+    const token = await sign({_id: newUser._id}, process.env.TOKEN);
+
+    return res.send({
+        user: newUser, token
+    })
 }
--- jasem/src/main.ts
+++ jasem/src/main.ts
@@ -1,9 +1,7 @@
 import * as express from 'express';
 import {connect} from "mongoose";
 import * as bodyParser from "body-parser";
-import {User} from "./mongo/User";
-import {compare, genSalt, hash} from "bcrypt";
-import {sign, verify} from "jsonwebtoken";
+import {login, register} from "./auth";
 require('dotenv').config();
 
 const app = express();
@@ -15,47 +13,9 @@ app.get('/status', (req, res) => {
     });
 });
 
-app.post('/register', async (req, res) => {
-    const {username, password: pass, name} = req.body;
+app.post('/register', register);
 
-    const user = await User.findOne({username});
-
-    if (user) return res.status(400).send({message: 'username exists!'});
-
-    // create new user
-    const password = await hash(pass, await genSalt(10));
-
-    const newUser = await User.create({
-        username, name, password
-    });
-
-    // generate token
-    const token = await sign({_id: newUser._id}, process.env.TOKEN);
-
-    return res.send({
-        user: newUser, token
-    })
-});
-
-app.post('/login', async (req, res) => {
-    const {username, password} = req.body;
-
-    const user = await User.findOne({username});
-
-    if (!user) return res.status(400).send({message: 'wrong info'});
-
-    // check for password
-    const passwordIsTrue = await compare(password, user.password);
-
-    if (!passwordIsTrue) return res.status(400).send({message: 'wrong info'});
-
-    // generate token
-    const token = await sign({_id: user._id}, process.env.TOKEN);
-
-    return res.send({
-        user: user, token
-    })
-});
+app.post('/login', login);
 
 connect(process.env.DB || 'mongodb://admin:secret@localhost:27017/noodle?authSource=admin', {useUnifiedTopology: true, useNewUrlParser: true}, () => {
     console.log('connected to db')
