a2f5d28fd096b825eafdb4be4cc3554031742c55
mohammad javad <mjkmo123@gmail.com>
2021-04-09 21:22:04 +0430

    map the labels

--- artorito/src/Dashboard.tsx
+++ artorito/src/Dashboard.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import DashboardCard from "./components/LabelCard";
+import {useLabelsStore} from "./stores/LabelsStore";
 
 
 export default function Dashboard() {
 
+    const {labels} = useLabelsStore();
+
     return (
         <div className="row" style={{gap: '10px'}}>
-            <DashboardCard title="21 H" description="Math"/>
-            <DashboardCard title="1 H" description="Biology"/>
-            <DashboardCard title="10 H" description="Computer"/>
+
+            {labels.map((data, index) => <DashboardCard title="21 H" description="Math" key={index}/>)}
+
         </div>
     )
 }
