a1c03ba52a63676a483bcf370245e41617292013
mohammad javad <mjkmo123@gmail.com>
2021-04-09 17:48:38 +0430

    add theme

--- artorito/src/App.tsx
+++ artorito/src/App.tsx
@@ -12,16 +12,44 @@ function App() {
     const {token} =  useTokenStore();
     const {theme: themeSaved} = useThemeStore();
 
-    const theme = createMuiTheme({
+    const dark = createMuiTheme({
         palette: {
-            // @ts-ignore
-            type: themeSaved
+            type: 'dark',
+            background: {
+                default: '#0f0e17'
+            },
+            secondary: {
+                main: '#ff8906',
+                contrastText: '#fffffe'
+            },
+            primary: {
+                main: '#e53170',
+                contrastText: '#fffffe'
+            },
+        },
+    });
+
+
+    const light = createMuiTheme({
+        palette: {
+            type: 'light',
+            background: {
+                default: '#fffffe'
+            },
+            secondary: {
+                main: '#ffd803',
+                contrastText: '#2d334a'
+            },
+            primary: {
+                main: '#bae8e8',
+                contrastText: '#2d334a'
+            }
         }
     });
 
     return (
         <div>
-            <ThemeProvider theme={theme}>
+            <ThemeProvider theme={themeSaved === 'light' ? light : dark}>
                 <CssBaseline />
                 {token !== '' ? <Main /> : <Login />}
                 <ConnectionLost />
