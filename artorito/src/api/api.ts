53b60afc58635e33c75ac3c00acedab94a8b530c
mohammad javad <mjkmo123@gmail.com>
2021-04-09 13:23:13 +0430

    add login function

--- artorito/src/api/api.ts
+++ artorito/src/api/api.ts
@@ -5,3 +5,7 @@ const axios = require('axios').default;
 export function status(){
     return axios.get(ENDPOINT + '/status')
 }
+
+export function login(username: string, password: string){
+    return axios.post(ENDPOINT + '/login', {username, password});
+}
