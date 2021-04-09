c1197ead1af54a404ddabf71b42b9299779ded78
mohammad javad <mjkmo123@gmail.com>
2021-04-09 21:22:35 +0430

    bug fix

--- artorito/src/Main.tsx
+++ artorito/src/Main.tsx
@@ -17,6 +17,7 @@ export default function Main() {
                 setLabels(res.data.labels);
             })
             .catch(console.log)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (
