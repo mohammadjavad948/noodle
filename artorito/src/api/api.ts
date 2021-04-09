cf0b89a7a3d30e0e3703803c4218fe1c9e90bd03
mohammad javad <mjkmo123@gmail.com>
2021-04-09 13:29:15 +0430

    add register api

--- artorito/src/api/api.ts
+++ artorito/src/api/api.ts
@@ -9,3 +9,7 @@ export function status(){
 export function login(username: string, password: string){
     return axios.post(ENDPOINT + '/login', {username, password});
 }
+
+export function register(username: string, password: string, name: string){
+    return axios.post(ENDPOINT + '/register', {username, password, name});
+}
