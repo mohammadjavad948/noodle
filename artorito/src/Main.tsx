e9401f33227c83ac26a76e22f50e035da7b6b2f8
mohammad javad <mjkmo123@gmail.com>
2021-04-09 21:16:33 +0430

    add useEffect

--- artorito/src/Main.tsx
+++ artorito/src/Main.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Dashboard from "./Dashboard";
 
 export default function Main() {
+
+    useEffect(() => {
+
+    }, []);
+
     return (
         <BrowserRouter>
             <Navbar />
