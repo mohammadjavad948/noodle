819d9a6dee40e868d0445f3060b113470c475097
mohammad javad <mjkmo123@gmail.com>
2021-04-10 08:35:48 +0430

    add state for form

--- artorito/src/Dashboard.tsx
+++ artorito/src/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import DashboardCard from "./components/LabelCard";
 import {useLabelsStore} from "./stores/LabelsStore";
 import {Button, Card, TextField, Typography} from "@material-ui/core";
@@ -20,18 +20,25 @@ export default function Dashboard() {
 
 function NewLabelCard(){
 
+    const [name, setName] = useState('');
+    const [color, setColor] = useState('#000000');
+
+    function save(){
+        console.log({name, color})
+    }
+
     return (
         <Card
             variant={"outlined"}
             className="col-12 col-sm-12 col-md-5 col-lg-3 col-xxl-3 d-flex flex-column align-items-center justify-content-center"
         >
             <Typography variant={"body1"} className="mt-2">new Label</Typography>
-            <TextField label="name" variant="outlined" className="mt-4" />
+            <TextField label="name" variant="outlined" onChange={(e) =>  {setName(e.target.value)}} className="mt-4" />
             <div className="mt-2">
                 color
-                <input type="color" onChange={(e) => {console.log(e.target.value)}}/>
+                <input type="color" onChange={(e) => {setColor(e.target.value)}}/>
             </div>
-            <Button variant="contained" color="primary" className="mt-2 mb-2">
+            <Button variant="contained" color="primary" onClick={save} className="mt-2 mb-2">
                 save
             </Button>
         </Card>
