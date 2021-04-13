81c003159f4b0baa15d2abcf8e9fde5036763c3e
mohammad javad <mjkmo123@gmail.com>
2021-04-13 14:20:30 +0430

    add big button

--- artorito/src/NewTime.tsx
+++ artorito/src/NewTime.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import style from './NewTime.module.css';
+import {Button, Icon} from "@material-ui/core";
+import {green} from "@material-ui/core/colors";
 
 export default function NewTime(){
 
     return (
         <div className={style.container}>
-
+            <BigStart />
         </div>
+    )
+}
+
+function BigStart(){
+
+    return (
+        <Button size={"large"} variant={"outlined"}>
+            <Icon fontSize={"large"} style={{color: green[500]}}>
+                play_arrow
+            </Icon>
+        </Button>
     )
 }
