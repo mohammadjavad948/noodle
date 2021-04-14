85fd0bf91c1ec48192c79a5a8b0c4213511af034
mohammad javad <mjkmo123@gmail.com>
2021-04-14 11:40:40 +0430

    hidden animation

--- artorito/src/components/TimeManager.tsx
+++ artorito/src/components/TimeManager.tsx
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {animated, useSpring} from 'react-spring';
 import {useTimeStore} from "../stores/TimeStore";
 import style from './TimeManager.module.css';
 import {Icon, IconButton} from "@material-ui/core";
+import {useStartStore} from "../stores/StartStore";
 
 export default function TimeManager(){
 
     const {time} = useTimeStore();
+    const {start} = useStartStore();
+
+    const [hidden, setHidden] = useState(true);
 
     const animation = useSpring({
-        display: time === 0 ? 'none' : 'block'
+        display: time === 0 ? 'none' : 'block',
+        right: hidden ? '-175px' : '10px'
     });
+
+    function changeHidden(){
+        setHidden(!hidden);
+    }
 
     return (
         <animated.div className={style.container} style={animation}>
+            <IconButton onClick={changeHidden}>
+                {
+                    hidden ? <Icon fontSize={"small"}>
+                        keyboard_arrow_left
+                    </Icon> :
+                        <Icon fontSize={"small"}>
+                            keyboard_arrow_right
+                        </Icon>
+                }
+            </IconButton>
             <IconButton>
                 <Icon fontSize={"small"}>
                     stop
