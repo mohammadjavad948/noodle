f0f4cc6311cd5ce46312a6035abda6aa0ac961e2
mohammad javad <mjkmo123@gmail.com>
2021-04-14 11:24:00 +0430

    import dep

--- artorito/src/components/TimeManager.tsx
+++ artorito/src/components/TimeManager.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import {animated, useSpring} from 'react-spring';
+import {useTimeStore} from "../stores/TimeStore";
 
 export default function TimeManager(){
+
+    const {time} = useTimeStore();
+
+    const animation = useSpring({
+
+    });
 
     return (
         <animated.div>
