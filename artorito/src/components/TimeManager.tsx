cd9c88ee65ed5c208958de5954a22d3c271181b2
mohammad javad <mjkmo123@gmail.com>
2021-04-14 11:34:18 +0430

    time manager style

--- artorito/src/components/TimeManager.tsx
+++ artorito/src/components/TimeManager.tsx
@@ -2,18 +2,38 @@ import React from 'react';
 import {animated, useSpring} from 'react-spring';
 import {useTimeStore} from "../stores/TimeStore";
 import style from './TimeManager.module.css';
+import {Icon, IconButton} from "@material-ui/core";
 
 export default function TimeManager(){
 
     const {time} = useTimeStore();
 
     const animation = useSpring({
-
+        display: time === 0 ? 'none' : 'block'
     });
 
     return (
-        <animated.div className={style.container}>
-            test
+        <animated.div className={style.container} style={animation}>
+            <IconButton>
+                <Icon fontSize={"small"}>
+                    stop
+                </Icon>
+            </IconButton>
+            <IconButton>
+                <Icon fontSize={"small"}>
+                    replay
+                </Icon>
+            </IconButton>
+            <IconButton>
+                <Icon fontSize={"small"}>
+                    done
+                </Icon>
+            </IconButton>
+            <IconButton>
+                <Icon fontSize={"small"}>
+                    play_arrow
+                </Icon>
+            </IconButton>
         </animated.div>
     )
 }
