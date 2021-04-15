422b20db3dc6bd5242974be473721f95d9b0383f
mohammad javad <mjkmo123@gmail.com>
2021-04-15 10:12:37 +0430

    create IO

--- jasem/src/main.ts
+++ jasem/src/main.ts
@@ -113,4 +113,6 @@ connect(process.env.DB || 'mongodb://admin:secret@localhost:27017/noodle?authSou
 
 const server = app.listen(process.env.PORT || 3000, () => {
     console.log('server is running')
-})
+});
+
+const IO = socketIo(server);
