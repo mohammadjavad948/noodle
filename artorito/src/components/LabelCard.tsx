534013e5950494553647761579e5418362094c72
mohammad javad <mjkmo123@gmail.com>
2021-04-16 09:44:44 +0430

    remove chart

--- artorito/src/components/LabelCard.tsx
+++ artorito/src/components/LabelCard.tsx
@@ -1,6 +1,5 @@
 import React, {useEffect, useRef, useState} from "react";
 import {Card, CardContent, Typography, useTheme} from "@material-ui/core";
-import Chart from "chart.js";
 import ContextMenu from "./Menu";
 
 interface DashboardCardI {
@@ -9,64 +8,15 @@ interface DashboardCardI {
 
 export default function DashboardCard(props: DashboardCardI) {
 
-    const graphCanvas = useRef() as React.MutableRefObject<HTMLCanvasElement>;
-
     const theme = useTheme();
 
     const [mouse, setMouse] = useState({open: false, x: 0, y: 0});
-    const [chartData, setChartData] = useState([0, 0, 0, 0, 0, 0, 0]);
     const [sum, setSum] = useState('');
 
     useEffect(() => {
-        buildChart();
-        createChartData();
         createSum();
     });
 
-    function buildChart() {
-        let canvas = graphCanvas.current.getContext('2d');
-
-        // @ts-ignore
-        new Chart(canvas, {
-            type: 'line',
-            data: {
-                labels: [1, 2, 3, 4, 5, 6, 7],
-                datasets: [{
-                    label: props.data.name,
-                    fill: false,
-                    borderColor: props.data.color,
-                    data: chartData
-                }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        gridLines: {
-                            display: false
-                        },
-                        ticks: {
-                            display: false
-                        }
-                    }],
-                    xAxes: [{
-                        gridLines: {
-                            display: false,
-                        },
-                        ticks: {
-                            display: false
-                        }
-                    }],
-                },
-                legend: {
-                    display: false
-                },
-                animation: false,
-                tooltips: {
-                    enabled: false
-                }
-            }
-        })
-    }
 
     function createSum(){
         let sum = props.data.time.reduce((n: any, next: any) => {
@@ -97,47 +47,6 @@ export default function DashboardCard(props: DashboardCardI) {
         return output
     }
 
-    function sortByDate(data: any){
-        let finalObj = {}
-        data.forEach((games: any) => {
-            const date = games.date.split('T')[0]
-            // @ts-ignore
-            if (finalObj[date]) {
-                // @ts-ignore
-                finalObj[date].push(games);
-            } else {
-                // @ts-ignore
-                finalObj[date] = [games];
-            }
-        });
-
-        return finalObj;
-    }
-
-    function createChartData(){
-        let final = [];
-        const sorted = sortByDate(props.data.time);
-
-        for (let i = 0; i < 7; i++){
-            let date: any = new Date(new Date().setDate(new Date().getDate() - i))
-
-            date = date.toISOString().split('T')[0]
-
-            // @ts-ignore
-            if (sorted[date]){
-                // @ts-ignore
-                const sum = sorted[date].reduce((a: any, b: any) => {
-                    return a + b.time
-                }, 0)
-
-                final.push(sum);
-            }else {
-                final.push(0);
-            }
-        }
-
-        setChartData(final);
-    }
 
     function contextM(e: any){
         e.preventDefault();
@@ -181,10 +90,6 @@ export default function DashboardCard(props: DashboardCardI) {
                     {props.data.name}
                 </Typography>
                 <Typography style={{textAlign: 'center'}}>{sum}</Typography>
-                <canvas
-                    width={"100%"}
-                    ref={graphCanvas}
-                />
             </CardContent>
             {mouse.open ? <ContextMenu content={contextMenu} hide={hideContext} {...mouse}/> : ''}
         </Card>
