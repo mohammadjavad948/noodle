2912495af0b39cc6a0cbe6904e59d00c78a2a10a
mohammad javad <mjkmo123@gmail.com>
2021-04-16 09:46:31 +0430

    remove unused import

--- artorito/src/components/LabelCard.tsx
+++ artorito/src/components/LabelCard.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {Card, CardContent, Typography, useTheme} from "@material-ui/core";
 import ContextMenu from "./Menu";
 
