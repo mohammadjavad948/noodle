91f576f815305bbb817f8b2ff6d054e4c76c95a8
mohammadjavad <mjkmo123@gmail.com>
2021-04-21 14:20:30 +0430

    add times

--- artorito/src/Label.tsx
+++ artorito/src/Label.tsx
@@ -10,6 +10,19 @@ export default function Label(){
     const {labels} = useLabelsStore();
     const label = labels.find(e => e._id === id);
 
+    function msToTime(duration: number) {
+        let milliseconds = (duration % 1000) / 100,
+            seconds: string | number = Math.floor((duration / 1000) % 60),
+            minutes: string | number = Math.floor((duration / (1000 * 60)) % 60),
+            hours: string | number = Math.floor((duration / (1000 * 60 * 60)) % 24);
+
+        hours = (hours < 10) ? "0" + hours : hours;
+        minutes = (minutes < 10) ? "0" + minutes : minutes;
+        seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+        return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+    }
+
     return (
         <div>
             <div className={style.title}>
@@ -17,6 +30,9 @@ export default function Label(){
                     <rect width="20" height="20" style={{fill: label?.color}} />
                 </svg>
                 <h1>{label?.name ?? "not found"}</h1>
+            </div>
+            <div>
+                {label?.time.map((el, index) => <div key={index}>{msToTime(el.time)}</div>)}
             </div>
         </div>
     )
