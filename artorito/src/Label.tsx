a926d4340c584dc1140e27d93935c53022acc159
mohammadjavad <mjkmo123@gmail.com>
2021-04-21 14:10:20 +0430

    add label module

--- artorito/src/Label.tsx
+++ artorito/src/Label.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import {useParams} from 'react-router-dom';
+import {useLabelsStore} from "./stores/LabelsStore";
 
 export default function Label(){
 
+    // @ts-ignore
+    const {id} = useParams();
+    const {labels} = useLabelsStore();
+    const label = labels.find(e => e._id === id);
+
     return (
         <div>
-
+            <div>
+                <h1>{label?.name ?? "not found"}</h1>
+            </div>
         </div>
     )
 }
