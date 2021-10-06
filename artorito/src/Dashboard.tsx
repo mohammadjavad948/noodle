34e6d5dcf8278223ef26c903846f86c6a726e5c9
mohammadjavad <mjkmo123@gmail.com>
2021-10-06 14:43:35 +0330

    color

--- artorito/src/Dashboard.tsx
+++ artorito/src/Dashboard.tsx
@@ -4,7 +4,7 @@ import {useLabelsStore} from "./stores/LabelsStore";
 import {Button, Card, TextField, Typography, useTheme} from "@material-ui/core";
 import {useTokenStore} from "./stores/TokenStore";
 import {createLabel} from "./api/api";
-
+import { TwitterPicker } from 'react-color'
 
 export default function Dashboard() {
 
@@ -42,14 +42,18 @@ function NewLabelCard(){
         <div className={"col-12 col-sm-12 col-md-5 col-lg-3 col-xxl-3 "}>
             <Card
                 variant={"outlined"}
-                className="d-flex flex-column align-items-center justify-content-center"
-                style={{background: theme.palette.background.default}}
+                className="d-flex flex-column align-items-center"
+                style={{background: theme.palette.background.default, padding: '10px'}}
             >
                 <Typography variant={"body1"} className="mt-2">new Label</Typography>
                 <TextField label="name" variant="outlined" onChange={(e) =>  {setName(e.target.value)}} className="mt-4" />
-                <div className="mt-2">
-                    color
-                    <input type="color" onChange={(e) => {setColor(e.target.value)}}/>
+                <div className="mt-2 d-flex align-items-center justify-content-center">
+                    <TwitterPicker
+                        color={color}
+                        onChange={(c) => setColor(c.hex)}
+                        triangle={"hide"}
+                        className={"color"}
+                    />
                 </div>
                 <Button variant="contained" color="primary" onClick={save} className="mt-2 mb-2">
                     save
